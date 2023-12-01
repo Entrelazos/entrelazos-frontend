1cd3ab7961c81411067d45a2b59b0bb9b0211cd7
Felipe Marin <lfelipe.marin@gmail.com>
2023-12-01 11:35:04 -0500

    remove unnecessary named export

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import { ThemeProvider } from 'styled-components';
 import { CartContextProvider } from './contexts/CartContext';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
-import { Router } from './Router';
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
-import { AppRouter } from './router/AppRouter';
+import AppRouter from './router/AppRouter';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 
