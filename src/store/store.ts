aee28818ad1c4dad754067a67c5527c19fa3f34b
Felipe Marin <lfelipe.marin@gmail.com>
2024-05-31 12:37:37 -0500

    persisting auth state

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,7 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import logger from 'redux-logger';
 import { authSlice } from './auth';
 import companiesSlice from './companies/companiesSlice';
 import companyService from '../services/companies/companyService';
@@ -10,9 +13,26 @@ import { setupInterceptors } from '../utils/common';
 import { citiesSlice, regionsSlice, countriesSlice } from './geo/geoSlice';
 import geoService from '../services/geo/geoService';
 
+// Combine reducers
+const rootReducer = combineReducers({
+  auth: authSlice.reducer,
+  // Add other reducers here if you have them
+});
+
+// Configure persist options
+const persistConfig = {
+  key: 'root', // Root key for the persisted state
+  storage, // Storage method (e.g., localStorage, AsyncStorage)
+  whitelist: ['auth'], // List of reducers to persist
+};
+
+// Create persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure store with persisted reducers
 export const store = configureStore({
   reducer: {
-    auth: authSlice.reducer,
+    auth: persistedReducer,
     companies: companiesSlice,
     categories: categoriesSlice,
     products: productsSlice,
@@ -20,6 +40,13 @@ export const store = configureStore({
     regions: regionsSlice.reducer,
     cities: citiesSlice.reducer,
   },
+});
+
+export const persistor = persistStore(store);
+
+// Debugging
+persistor.subscribe(() => {
+  console.log('Rehydrated state:', store.getState());
 });
 
 setupInterceptors(companyService);
